42de25f5d490b09e64ea160bddb25e64417fd94f
Ademola <oladipupoademola09@mail.com>
2024-06-01 14:36:23 +0100

    Personal Portfolio Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ["./*.html"],
   theme: {
     extend: {
-   screens:{
-    "sm":"480px"
-   },
    spacing:{
     "big":"48rem"
    }
+ },
+ screens:{
+  sm:"480px",
+  md:"768px",
+  lg:"1024px",
+  xl:"1280px",
  },
  
   fontFamily:{
